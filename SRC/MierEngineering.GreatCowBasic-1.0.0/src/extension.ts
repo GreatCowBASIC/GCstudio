a59e85cb34c93c54d2f5fc0df352fe12413da5f6
Angel Mier <angelivan_spartan@hotmail.com>
2022-03-19 14:25:16 -0600

    fassoc tool and symbol comment fix

--- SRC/MierEngineering.GreatCowBasic-1.0.0/src/extension.ts
+++ SRC/MierEngineering.GreatCowBasic-1.0.0/src/extension.ts
@@ -15,91 +15,175 @@ class GCBDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
 
         return new Promise((resolve, reject) => {
             var symbols:any = [];
-
+            var remblock:boolean = false;
 
 
 
             for (var i = 0; i < document.lineCount; i++) {
                 var line = document.lineAt(i);
                 var regex = new RegExp("(?:#chip\\s+)(\\S+)","i");
-                
-                if (regex.test(line.text)) {                  
-                    var symname = regex.exec(line.text);
-                    symbols.push({
-                        name: "Part " + symname![1],
-                        kind: vscode.SymbolKind.Class,
-                        location: new vscode.Location(document.uri, line.range)
-                    })
+                var remregex = new RegExp("(?:[';]|rem).*#chip","i");
+                var startremblockregex = new RegExp("[/][*]","i");
+                var endremblockregex = new RegExp("[*][/]","i");
+                if (startremblockregex.test(line.text))
+                {
+                    remblock = true;
+                }
+                if (endremblockregex.test(line.text))
+                {
+                    remblock = false;
+                }
+
+                if (!remregex.test(line.text) && !remblock)
+                {
+                    if (regex.test(line.text)) {                  
+                        var symname = regex.exec(line.text);
+                        symbols.push({
+                            name: "Part " + symname![1],
+                            kind: vscode.SymbolKind.Class,
+                            location: new vscode.Location(document.uri, line.range)
+                        })
+                    }
                 }
             }
 
             for (var i = 0; i < document.lineCount; i++) {
                 var line = document.lineAt(i);
                 var regex = new RegExp("(?:#config\\s+)(\\S+)","i");
-                
-                if (regex.test(line.text)) {                  
-                    var symname = regex.exec(line.text);
-                    symbols.push({
-                        name: symname![1],
-                        kind: vscode.SymbolKind.Property,
-                        location: new vscode.Location(document.uri, line.range)
-                    })
+                var remregex = new RegExp("(?:[';]|rem).*#config","i");
+                var startremblockregex = new RegExp("[/][*]","i");
+                var endremblockregex = new RegExp("[*][/]","i");
+                if (startremblockregex.test(line.text))
+                {
+                    remblock = true;
+                }
+                if (endremblockregex.test(line.text))
+                {
+                    remblock = false;
+                }
+
+                if (!remregex.test(line.text) && !remblock)
+                {
+                    if (regex.test(line.text)) {                  
+                        var symname = regex.exec(line.text);
+                        symbols.push({
+                            name: symname![1],
+                            kind: vscode.SymbolKind.Property,
+                            location: new vscode.Location(document.uri, line.range)
+                        })
+                    }
                 }
             }
 
             for (var i = 0; i < document.lineCount; i++) {
                 var line = document.lineAt(i);
                 var regex = new RegExp("(?:#define\\s+)(\\S+)","i");
-                
-                if (regex.test(line.text)) {                  
-                    var symname = regex.exec(line.text);
-                    symbols.push({
-                        name: symname![1],
-                        kind: vscode.SymbolKind.Constant,
-                        location: new vscode.Location(document.uri, line.range)
-                    })
+                var remregex = new RegExp("(?:[';]|rem).*#define","i");
+                var startremblockregex = new RegExp("[/][*]","i");
+                var endremblockregex = new RegExp("[*][/]","i");
+                if (startremblockregex.test(line.text))
+                {
+                    remblock = true;
+                }
+                if (endremblockregex.test(line.text))
+                {
+                    remblock = false;
+                }
+
+                if (!remregex.test(line.text) && !remblock)
+                {
+                    if (regex.test(line.text)) {                  
+                        var symname = regex.exec(line.text);
+                        symbols.push({
+                            name: symname![1],
+                            kind: vscode.SymbolKind.Constant,
+                            location: new vscode.Location(document.uri, line.range)
+                        })
+                    }
                 }
             }
 
             for (var i = 0; i < document.lineCount; i++) {
                 var line = document.lineAt(i);
                 var regex = new RegExp("(?:\\bdim\\s+)(\\S+)","i");
-                
-                if (regex.test(line.text)) {                  
-                    var symname = regex.exec(line.text);
-                    symbols.push({
-                        name: symname![1],
-                        kind: vscode.SymbolKind.Variable,
-                        location: new vscode.Location(document.uri, line.range)
-                    })
+                var remregex = new RegExp("(?:[';]|rem).*dim","i");
+                var startremblockregex = new RegExp("[/][*]","i");
+                var endremblockregex = new RegExp("[*][/]","i");
+                if (startremblockregex.test(line.text))
+                {
+                    remblock = true;
+                }
+                if (endremblockregex.test(line.text))
+                {
+                    remblock = false;
+                }
+
+                if (!remregex.test(line.text) && !remblock)
+                {
+                    if (regex.test(line.text)) {                  
+                        var symname = regex.exec(line.text);
+                        symbols.push({
+                            name: symname![1],
+                            kind: vscode.SymbolKind.Variable,
+                            location: new vscode.Location(document.uri, line.range)
+                        })
+                    }
                 }
             }
 
             for (var i = 0; i < document.lineCount; i++) {
                 var line = document.lineAt(i);
                 var regex = new RegExp("(?:\\bdir\\s+)(\\S+)","i");
-                
-                if (regex.test(line.text)) {                  
-                    var symname = regex.exec(line.text);
-                    symbols.push({
-                        name: symname![1],
-                        kind: vscode.SymbolKind.Event,
-                        location: new vscode.Location(document.uri, line.range)
-                    })
+                var remregex = new RegExp("(?:[';]|rem).*dir","i");
+                var startremblockregex = new RegExp("[/][*]","i");
+                var endremblockregex = new RegExp("[*][/]","i");
+                if (startremblockregex.test(line.text))
+                {
+                    remblock = true;
+                }
+                if (endremblockregex.test(line.text))
+                {
+                    remblock = false;
+                }
+
+                if (!remregex.test(line.text) && !remblock)
+                {
+                    if (regex.test(line.text)) {                  
+                        var symname = regex.exec(line.text);
+                        symbols.push({
+                            name: symname![1],
+                            kind: vscode.SymbolKind.Event,
+                            location: new vscode.Location(document.uri, line.range)
+                        })
+                    }
                 }
             }
 
             for (var i = 0; i < document.lineCount; i++) {
                 var line = document.lineAt(i);
                 var regex = new RegExp("\\b\\s*(\\S+)(?::)$","i");
-                
-                if (regex.test(line.text)) {                  
-                    var symname = regex.exec(line.text);
-                    symbols.push({
-                        name: symname![1],
-                        kind: vscode.SymbolKind.Namespace,
-                        location: new vscode.Location(document.uri, line.range)
-                    })
+                var remregex = new RegExp("(?:[';]|rem).*:","i");
+                var startremblockregex = new RegExp("[/][*]","i");
+                var endremblockregex = new RegExp("[*][/]","i");
+                if (startremblockregex.test(line.text))
+                {
+                    remblock = true;
+                }
+                if (endremblockregex.test(line.text))
+                {
+                    remblock = false;
+                }
+
+                if (!remregex.test(line.text) && !remblock)
+                {                
+                    if (regex.test(line.text)) {                  
+                        var symname = regex.exec(line.text);
+                        symbols.push({
+                            name: symname![1],
+                            kind: vscode.SymbolKind.Namespace,
+                            location: new vscode.Location(document.uri, line.range)
+                        })
+                    }
                 }
             }
 
@@ -107,42 +191,84 @@ class GCBDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
             for (var i = 0; i < document.lineCount; i++) {
                 var line = document.lineAt(i);
                 var regex = new RegExp("(?:\\bsub\\s+)(\\S+)","i");
-                
-                if (regex.test(line.text)) {                  
-                    var symname = regex.exec(line.text);
-                    symbols.push({
-                        name: symname![1],
-                        kind: vscode.SymbolKind.Method,
-                        location: new vscode.Location(document.uri, line.range)
-                    })
+                var remregex = new RegExp("(?:[';]|rem).*sub","i");
+                var startremblockregex = new RegExp("[/][*]","i");
+                var endremblockregex = new RegExp("[*][/]","i");
+                if (startremblockregex.test(line.text))
+                {
+                    remblock = true;
+                }
+                if (endremblockregex.test(line.text))
+                {
+                    remblock = false;
+                }
+
+                if (!remregex.test(line.text) && !remblock)
+                {
+                    if (regex.test(line.text)) {                  
+                        var symname = regex.exec(line.text);
+                        symbols.push({
+                            name: symname![1],
+                            kind: vscode.SymbolKind.Method,
+                            location: new vscode.Location(document.uri, line.range)
+                        })
+                    }
                 }
             }
 
             for (var i = 0; i < document.lineCount; i++) {
                 var line = document.lineAt(i);
                 var regex = new RegExp("(?:\\bmacro\\s+)(\\S+)","i");
-                
-                if (regex.test(line.text)) {                  
-                    var symname = regex.exec(line.text);
-                    symbols.push({
-                        name: symname![1],
-                        kind: vscode.SymbolKind.Method,
-                        location: new vscode.Location(document.uri, line.range)
-                    })
+                var remregex = new RegExp("(?:[';]|rem).*macro","i");
+                var startremblockregex = new RegExp("[/][*]","i");
+                var endremblockregex = new RegExp("[*][/]","i");
+                if (startremblockregex.test(line.text))
+                {
+                    remblock = true;
+                }
+                if (endremblockregex.test(line.text))
+                {
+                    remblock = false;
+                }
+
+                if (!remregex.test(line.text) && !remblock)
+                {
+                    if (regex.test(line.text)) {                  
+                        var symname = regex.exec(line.text);
+                        symbols.push({
+                            name: symname![1],
+                            kind: vscode.SymbolKind.Method,
+                            location: new vscode.Location(document.uri, line.range)
+                        })
+                    }
                 }
             }
 
             for (var i = 0; i < document.lineCount; i++) {
                 var line = document.lineAt(i);
                 var regex = new RegExp("(?:\\bfunction\\s+)(\\S+)","i");
-                
-                if (regex.test(line.text)) {                  
-                    var symname = regex.exec(line.text);
-                    symbols.push({
-                        name: symname![1],
-                        kind: vscode.SymbolKind.Function,
-                        location: new vscode.Location(document.uri, line.range)
-                    })
+                var remregex = new RegExp("(?:[';]|rem).*function","i");
+                var startremblockregex = new RegExp("[/][*]","i");
+                var endremblockregex = new RegExp("[*][/]","i");
+                if (startremblockregex.test(line.text))
+                {
+                    remblock = true;
+                }
+                if (endremblockregex.test(line.text))
+                {
+                    remblock = false;
+                }
+
+                if (!remregex.test(line.text) && !remblock)
+                {                
+                    if (regex.test(line.text)) {                  
+                        var symname = regex.exec(line.text);
+                        symbols.push({
+                            name: symname![1],
+                            kind: vscode.SymbolKind.Function,
+                            location: new vscode.Location(document.uri, line.range)
+                        })
+                    }
                 }
             }
 
@@ -150,28 +276,56 @@ class GCBDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
             for (var i = 0; i < document.lineCount; i++) {
                 var line = document.lineAt(i);
                 var regex = new RegExp("(?:\\btable\\s+)(\\S+)","i");
-                
-                if (regex.test(line.text)) {                  
-                    var symname = regex.exec(line.text);
-                    symbols.push({
-                        name: symname![1],
-                        kind: vscode.SymbolKind.Interface,
-                        location: new vscode.Location(document.uri, line.range)
-                    })
+                var remregex = new RegExp("(?:[';]|rem).*table","i");
+                var startremblockregex = new RegExp("[/][*]","i");
+                var endremblockregex = new RegExp("[*][/]","i");
+                if (startremblockregex.test(line.text))
+                {
+                    remblock = true;
+                }
+                if (endremblockregex.test(line.text))
+                {
+                    remblock = false;
+                }
+
+                if (!remregex.test(line.text) && !remblock)
+                {
+                    if (regex.test(line.text)) {                  
+                        var symname = regex.exec(line.text);
+                        symbols.push({
+                            name: symname![1],
+                            kind: vscode.SymbolKind.Interface,
+                            location: new vscode.Location(document.uri, line.range)
+                        })
+                    }
                 }
             }
 
             for (var i = 0; i < document.lineCount; i++) {
                 var line = document.lineAt(i);
                 var regex = new RegExp("(?:#script\\s+)(\\S+)","i");
-                
-                if (regex.test(line.text)) {                  
-                    var symname = regex.exec(line.text);
-                    symbols.push({
-                        name: symname![1],
-                        kind: vscode.SymbolKind.Key,
-                        location: new vscode.Location(document.uri, line.range)
-                    })
+                var remregex = new RegExp("(?:[';]|rem).*script","i");
+                var startremblockregex = new RegExp("[/][*]","i");
+                var endremblockregex = new RegExp("[*][/]","i");
+                if (startremblockregex.test(line.text))
+                {
+                    remblock = true;
+                }
+                if (endremblockregex.test(line.text))
+                {
+                    remblock = false;
+                }
+
+                if (!remregex.test(line.text) && !remblock)
+                {
+                    if (regex.test(line.text)) {                  
+                        var symname = regex.exec(line.text);
+                        symbols.push({
+                            name: symname![1],
+                            kind: vscode.SymbolKind.Key,
+                            location: new vscode.Location(document.uri, line.range)
+                        })
+                    }
                 }
             }
            
---
+++ SRC/fassoc/fasoc Script.nsi
@@ -0,0 +1,110 @@
+############################################################################################
+#                       NSIS File Associations Tool Script For GC Studio  V.1.01
+#                                   By Angel Mier                              
+############################################################################################
+
+######################################################################
+# Installer Configuration
+
+!define APP_NAME "GC Studio"
+!define COMP_NAME "Mier Engineering"
+!define WEB_SITE "https://www.gcbasic.com"
+!define VERSION "1.02.00.00"
+!define COPYRIGHT "Copyright © 2007 - 2022"
+!define DESCRIPTION "Application"
+!define INSTALLER_NAME ".\fassoc.exe"
+!define MAIN_APP_EXE "fassoc.exe"
+!define INSTALL_TYPE "SetShellVarContext current"
+!define REG_ROOT "HKCU"
+!define REG_CLASSES "HKCR"
+!define REG_APP_PATH "Software\Microsoft\Windows\CurrentVersion\App Paths\${MAIN_APP_EXE}"
+!define UNINSTALL_PATH "Software\Microsoft\Windows\CurrentVersion\Uninstall\${APP_NAME}"
+
+######################################################################
+# Bynary Information
+
+VIProductVersion  "${VERSION}"
+VIAddVersionKey "ProductName"  "${APP_NAME}"
+VIAddVersionKey "CompanyName"  "${COMP_NAME}"
+VIAddVersionKey "LegalCopyright"  "${COPYRIGHT}"
+VIAddVersionKey "FileDescription"  "${DESCRIPTION}"
+VIAddVersionKey "FileVersion"  "${VERSION}"
+
+######################################################################
+# Compression and installer settings
+
+SetCompressor LZMA
+Name "${APP_NAME}"
+Caption "${APP_NAME} File Associations Tool"
+OutFile "${INSTALLER_NAME}"
+BrandingText "${APP_NAME}"
+XPStyle on
+InstallDirRegKey "${REG_ROOT}" "${REG_APP_PATH}" ""
+InstallDir "$EXEDIR"
+
+
+######################################################################
+# Write Reg Keys
+Section -Icons_Reg
+
+#File Associations
+WriteRegStr ${REG_CLASSES} ".gcb" "" "GCB File"
+WriteRegStr ${REG_CLASSES} "GCB File\shell\open\command" ""  "$INSTDIR\GCstudio.exe $\"%1$\""
+WriteRegStr ${REG_CLASSES} "GCB File\Defaulticon" "" "$INSTDIR\GCstudio.exe,0"
+
+WriteRegStr ${REG_CLASSES} ".asm" "" "ASM File"
+WriteRegStr ${REG_CLASSES} "ASM File\shell\open\command" ""  "$INSTDIR\GCstudio.exe $\"%1$\""
+WriteRegStr ${REG_CLASSES} "ASM File\Defaulticon" "" "$INSTDIR\FileIcons\asm.ico,0"
+
+WriteRegStr ${REG_CLASSES} ".s" "" "S File"
+WriteRegStr ${REG_CLASSES} "S File\shell\open\command" ""  "$INSTDIR\GCstudio.exe $\"%1$\""
+WriteRegStr ${REG_CLASSES} "S File\Defaulticon" "" "$INSTDIR\FileIcons\asm.ico,0"
+
+WriteRegStr ${REG_CLASSES} ".bas" "" "FBasic File"
+WriteRegStr ${REG_CLASSES} "FBasic File\shell\open\command" ""  "$INSTDIR\GCstudio.exe $\"%1$\""
+WriteRegStr ${REG_CLASSES} "FBasic File\Defaulticon" "" "$INSTDIR\FileIcons\bas.ico,0"
+
+WriteRegStr ${REG_CLASSES} ".bi" "" "Fbasic Library"
+WriteRegStr ${REG_CLASSES} "Fbasic Library\shell\open\command" ""  "$INSTDIR\GCstudio.exe $\"%1$\""
+WriteRegStr ${REG_CLASSES} "Fbasic Library\Defaulticon" "" "$INSTDIR\FileIcons\h.ico,0"
+
+WriteRegStr ${REG_CLASSES} ".h" "" "GCB Library"
+WriteRegStr ${REG_CLASSES} "GCB Library\shell\open\command" ""  "$INSTDIR\GCstudio.exe $\"%1$\""
+WriteRegStr ${REG_CLASSES} "GCB Library\Defaulticon" "" "$INSTDIR\FileIcons\h.ico,0"
+
+WriteRegStr ${REG_CLASSES} ".json" "" "Json File"
+WriteRegStr ${REG_CLASSES} "Json File\shell\open\command" ""  "$INSTDIR\GCstudio.exe $\"%1$\""
+WriteRegStr ${REG_CLASSES} "Json File\Defaulticon" "" "$INSTDIR\FileIcons\json.ico,0"
+
+WriteRegStr ${REG_CLASSES} ".code-workspace" "" "GCB Project"
+WriteRegStr ${REG_CLASSES} "GCB Project\shell\open\command" ""  "$INSTDIR\GCstudio.exe $\"%1$\""
+WriteRegStr ${REG_CLASSES} "GCB Project\Defaulticon" "" "$INSTDIR\FileIcons\project.ico,0"
+
+
+#Windows Context Menu
+#shell
+WriteRegStr ${REG_CLASSES} "Directory\shell\GCstudio" "" "Open with GC Studio"
+WriteRegStr ${REG_CLASSES} "Directory\shell\GCstudio\command" ""  "$INSTDIR\GCstudio.exe $\"%V$\""
+WriteRegStr ${REG_CLASSES} "Directory\shell\GCstudio" "Icon" "$INSTDIR\GCstudio.exe,0"
+
+WriteRegStr ${REG_CLASSES} "Directory\Background\shell\GCstudio" "" "Open with GC Studio"
+WriteRegStr ${REG_CLASSES} "Directory\Background\shell\GCstudio\command" ""  "$INSTDIR\GCstudio.exe $\"%V$\""
+WriteRegStr ${REG_CLASSES} "Directory\Background\shell\GCstudio" "Icon" "$INSTDIR\GCstudio.exe,0"
+
+WriteRegStr ${REG_ROOT} "Software\Classes\Directory\Background\shell\GCstudio" "" "Open with GC Studio"
+WriteRegStr ${REG_ROOT} "Software\Classes\Directory\Background\shell\GCstudio\command" ""  "$INSTDIR\GCstudio.exe $\"%V$\""
+WriteRegStr ${REG_ROOT} "Software\Classes\Directory\Background\shell\GCstudio" "Icon" "$INSTDIR\GCstudio.exe,0"
+
+#drive
+WriteRegStr ${REG_CLASSES} "Drive\shell\GCstudio" "" "Open with GC Studio"
+WriteRegStr ${REG_CLASSES} "Drive\shell\GCstudio\command" ""  "$INSTDIR\GCstudio.exe $\"%V$\""
+WriteRegStr ${REG_CLASSES} "Drive\shell\GCstudio" "Icon" "$INSTDIR\GCstudio.exe,0"
+
+#files
+WriteRegStr ${REG_CLASSES} "*\shell\GCstudio" "" "Open with GC Studio"
+WriteRegStr ${REG_CLASSES} "*\shell\GCstudio\command" ""  "$INSTDIR\GCstudio.exe $\"%1$\""
+WriteRegStr ${REG_CLASSES} "*\shell\GCstudio" "Icon" "$INSTDIR\GCstudio.exe,0"
+
+SectionEnd
+
+######################################################################
