f7c842fb3920dd062c51c12ce53d7ab3ec4d68cd
angel-spartan <angelivan_spartan@hotmail.com>
2022-04-10 13:41:25 -0500

    Hover Converter code integrated to GCB extension.

--- Build/net6.0-windows/vscode/data/extensions/MierEngineering.GreatCowBasic-1.0.0/dist/extension.js
+++ Build/net6.0-windows/vscode/data/extensions/MierEngineering.GreatCowBasic-1.0.0/dist/extension.js
@@ -25,7 +25,6 @@ function activate(context) {
             }
         });
     }
-    vscode_1.commands.executeCommand("extension.enableHoverconverter");
     console.log("extension is now active!");
     // rest of code
     // Step: If simple commands then add to this array
@@ -180,6 +179,38 @@ function activate(context) {
         context.subscriptions.push(disposableUserButtonCommand);
     }
     //also update userButton in package.json.. see "Adding new userButtons" in help.md file
+    //Binary/Hex/Decimal Converter Code:
+    var regexHex = /^0x[0-9a-fA-F]+$/g;
+    var regexHexc = /^[0-9a-fA-F]+[h]$/g;
+    var regexDec = /^-?[0-9]+$/g;
+    var regexBin = /^0b[01]+$/g;
+    let hover = vscode.languages.registerHoverProvider({ scheme: '*', language: '*' }, {
+        provideHover(document, position, token) {
+            var hoveredWord = document.getText(document.getWordRangeAtPosition(position));
+            var markdownString = new vscode.MarkdownString();
+            if (regexBin.test(hoveredWord.toString())) {
+                var input = Number(parseInt(hoveredWord.substring(2), 2).toString());
+                markdownString.appendCodeblock(`Dec:\n${input}\nHex:\n0x${input.toString(16).toUpperCase()} `, 'javascript');
+                return {
+                    contents: [markdownString]
+                };
+            }
+            else if (regexHex.test(hoveredWord.toString()) || regexHexc.test(hoveredWord.toString())) {
+                markdownString.appendCodeblock(`Dec:\n${parseInt(hoveredWord, 16)}\nBinary:\n${parseInt(hoveredWord, 16).toString(2)}`, 'javascript');
+                return {
+                    contents: [markdownString]
+                };
+            }
+            else if (regexDec.test(hoveredWord.toString())) {
+                var input = Number(hoveredWord.toString());
+                markdownString.appendCodeblock(`Hex:\n0x${input.toString(16).toUpperCase()}\nBinary:\n${input.toString(2).replace(/(^\s+|\s+$)/, '')} `, 'javascript');
+                return {
+                    contents: [markdownString]
+                };
+            }
+        }
+    });
+    context.subscriptions.push(hover);
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
--- SRC/MierEngineering.GreatCowBasic-1.0.0/src/extension.ts
+++ SRC/MierEngineering.GreatCowBasic-1.0.0/src/extension.ts
@@ -1,6 +1,8 @@
 'use strict';
 import * as vscode from 'vscode';
 
+
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 // let fs = require("fs");
@@ -40,7 +42,6 @@ export function activate(context: vscode.ExtensionContext) {
       });
     }
   
-    commands.executeCommand("extension.enableHoverconverter")
     console.log("extension is now active!");
   
   
@@ -258,7 +259,50 @@ export function activate(context: vscode.ExtensionContext) {
     
       //also update userButton in package.json.. see "Adding new userButtons" in help.md file
 
-}
+
+    //Binary/Hex/Decimal Converter Code:
+	var regexHex = /^0x[0-9a-fA-F]+$/g;
+	var regexHexc = /^[0-9a-fA-F]+[h]$/g;
+	var regexDec = /^-?[0-9]+$/g;
+	var regexBin = /^0b[01]+$/g;
+	let hover = vscode.languages.registerHoverProvider({ scheme: '*', language: '*' }, {
+		provideHover(document, position, token) {
+			var hoveredWord = document.getText(document.getWordRangeAtPosition(position));
+			var markdownString = new vscode.MarkdownString();
+			if (regexBin.test(hoveredWord.toString())) {
+
+				var input: Number = Number(parseInt(hoveredWord.substring(2), 2).toString());
+				markdownString.appendCodeblock(`Dec:\n${input}\nHex:\n0x${input.toString(16).toUpperCase()} `, 'javascript');
+
+				return {
+					contents: [markdownString]
+				};
+			}
+			else if (regexHex.test(hoveredWord.toString()) || regexHexc.test(hoveredWord.toString())) {
+
+				markdownString.appendCodeblock(`Dec:\n${parseInt(hoveredWord, 16)}\nBinary:\n${parseInt(hoveredWord, 16).toString(2)}`, 'javascript');
+
+				return {
+					contents: [markdownString]
+				};
+			}
+			else if (regexDec.test(hoveredWord.toString())) {
+
+				var input: Number = Number(hoveredWord.toString());
+				markdownString.appendCodeblock(`Hex:\n0x${input.toString(16).toUpperCase()}\nBinary:\n${input.toString(2).replace(/(^\s+|\s+$)/, '')} `, 'javascript');
+
+				return {
+					contents: [markdownString]
+				};
+
+			}
+		}
+	});
+
+	context.subscriptions.push(hover);
+
+
+  }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
