ce3852ca90f02b0270366f9b646ef5c035904f4e
angelivan_spartan <angelivan_spartan@hotmail.com>
2022-05-25 00:43:02 -0500

    Finished Splash Screen Redesign

--- SRC/GCstudio/AboutBox.Designer.cs
+++ SRC/GCstudio/AboutBox.Designer.cs
@@ -64,7 +64,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(0, 34);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(686, 400);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
