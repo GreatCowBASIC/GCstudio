8684296e6a6c4c1b69501efa43bed07b4b65551f
angelivan_spartan <angelivan_spartan@hotmail.com>
2022-09-16 16:06:15 -0500

    ResetToFactory.exe updated

--- SRC/GCstudio/Properties/AssemblyInfo.cs
+++ SRC/GCstudio/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("00.34.0.120")]
-[assembly: AssemblyFileVersion("00.34.0.120")]
+[assembly: AssemblyVersion("00.34.0.121")]
+[assembly: AssemblyFileVersion("00.34.0.121")]
