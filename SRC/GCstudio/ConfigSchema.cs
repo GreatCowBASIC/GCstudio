97ff0c55eaf16e5d2c7877f437cf5c5954816c7d
angel-spartan <angelivan_spartan@hotmail.com>
2022-10-25 14:34:00 -0500

    Started Json implementation

---
+++ SRC/GCstudio/ConfigSchema.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GC_Studio
+{
+    class ConfigSchema
+    {
+
+        public string ReleaseChanel { get; set; }
+        public string IDE { get; set; }
+        public string architecture { get; set; }
+        public int sizeW { get; set; }
+        public int sizeH { get; set; }
+        public Int32 locx { get; set; }
+        public Int32 locy { get; set; }
+        public bool maximized { get; set; }
+
+
+    }
+}
