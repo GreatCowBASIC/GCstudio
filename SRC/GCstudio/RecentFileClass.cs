bdb61f2fef44791161670a677396b14c5c622967
angel-spartan <angelivan_spartan@hotmail.com>
2022-10-30 19:41:36 -0600

    Recent files schema and json working, added last directory to config

---
+++ SRC/GCstudio/RecentFileClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GC_Studio
+{
+    public class RecentFile
+    {
+        public string[] RecentName = new string[10];
+        public string[] RecentDir = new string[10];
+        public int RecentN = 0;
+    }
+}
