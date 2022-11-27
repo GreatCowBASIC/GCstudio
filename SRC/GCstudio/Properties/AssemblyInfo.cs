081eaa175825f77b4f025a23549ee8ea8fd6de57
angelivan_spartan <angelivan_spartan@hotmail.com>
2022-11-27 15:15:01 -0600

    more code for win7, changed v100 to v1

--- SRC/GCstudio/Properties/AssemblyInfo.cs
+++ SRC/GCstudio/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("100.00.01.157")]
-[assembly: AssemblyFileVersion("100.00.01.157")]
+[assembly: AssemblyVersion("1.00.01.160")]
+[assembly: AssemblyFileVersion("1.00.01.160")]
