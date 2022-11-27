51e26b79b83c26e7a621be98e9f45ed5c0e4e79d
angelivan_spartan <angelivan_spartan@hotmail.com>
2022-11-26 22:40:56 -0600

    removed process start, fixed synwrite first run file

--- SRC/GCstudio/Properties/AssemblyInfo.cs
+++ SRC/GCstudio/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("100.00.01.155")]
-[assembly: AssemblyFileVersion("100.00.01.155")]
+[assembly: AssemblyVersion("100.00.01.156")]
+[assembly: AssemblyFileVersion("100.00.01.156")]
