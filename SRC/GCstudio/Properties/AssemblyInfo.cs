a4f6c79d37d3a09b3f122049df85bba24724bd61
angelivan_spartan <angelivan_spartan@hotmail.com>
2023-03-20 19:28:31 -0600

    Build for v.1.00.101

--- SRC/GCstudio/Properties/AssemblyInfo.cs
+++ SRC/GCstudio/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.00.10.195")]
-[assembly: AssemblyFileVersion("1.00.10.195")]
+[assembly: AssemblyVersion("1.00.101.196")]
+[assembly: AssemblyFileVersion("1.00.101.196")]
