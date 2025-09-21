6722b8111a8fc694c9fec2f5d8ebce24e1258dcc
Angel Mier <angelivan_spartan@hotmail.com>
2025-09-21 14:58:54 -0600

    BuildFIX>removed compile and include resources from csproj
    added modules form

---
+++ SRC/GCstudio/Modules.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GC_Studio
+{
+    public partial class Modules : Form
+    {
+        public Modules()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDeploy_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
