6d01acd4238f1b5e6f01cec28f09d7847c53107b
Angel Mier <angelivan_spartan@hotmail.com>
2025-09-22 17:09:18 -0600

    Deprecated fassoc, now postupdater sets registry keys and needs to be exec as admin

--- SRC/GCstudio/Properties/AssemblyInfo.cs
+++ SRC/GCstudio/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.01.25.282")]
-[assembly: AssemblyFileVersion("1.01.25.282")]
+[assembly: AssemblyVersion("1.01.251.282")]
+[assembly: AssemblyFileVersion("1.01.251.282")]
