ea9db176259c8b13870b6509e51ba91c231867a9
Angel Mier <angelivan_spartan@hotmail.com>
2025-09-25 15:35:10 -0600

    ModulesForm: Added ModulesClass, generates/updates modules.json, produces debug logs

--- SRC/GCstudio/Modules.Designer.cs
+++ SRC/GCstudio/Modules.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             checkedListBoxModules = new System.Windows.Forms.CheckedListBox();
-            buttonDeploy = new System.Windows.Forms.Button();
+            buttonOk = new System.Windows.Forms.Button();
             buttonRem = new System.Windows.Forms.Button();
             SuspendLayout();
             // 
@@ -37,26 +37,29 @@
             // 
             checkedListBoxModules.BackColor = System.Drawing.Color.Silver;
             checkedListBoxModules.FormattingEnabled = true;
-            checkedListBoxModules.Location = new System.Drawing.Point(12, 12);
+            checkedListBoxModules.Location = new System.Drawing.Point(17, 20);
+            checkedListBoxModules.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             checkedListBoxModules.Name = "checkedListBoxModules";
-            checkedListBoxModules.Size = new System.Drawing.Size(374, 346);
+            checkedListBoxModules.Size = new System.Drawing.Size(533, 592);
             checkedListBoxModules.TabIndex = 0;
+            checkedListBoxModules.ThreeDCheckBoxes = true;
             // 
-            // buttonDeploy
+            // buttonOk
             // 
-            buttonDeploy.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
-            buttonDeploy.BackColor = System.Drawing.Color.DimGray;
-            buttonDeploy.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(25, 25, 25);
-            buttonDeploy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            buttonDeploy.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold);
-            buttonDeploy.ForeColor = System.Drawing.Color.White;
-            buttonDeploy.Location = new System.Drawing.Point(125, 377);
-            buttonDeploy.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            buttonDeploy.Name = "buttonDeploy";
-            buttonDeploy.Size = new System.Drawing.Size(125, 30);
-            buttonDeploy.TabIndex = 50;
-            buttonDeploy.Text = "Deploy Packages";
-            buttonDeploy.UseVisualStyleBackColor = false;
+            buttonOk.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
+            buttonOk.BackColor = System.Drawing.Color.DimGray;
+            buttonOk.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(25, 25, 25);
+            buttonOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            buttonOk.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold);
+            buttonOk.ForeColor = System.Drawing.Color.White;
+            buttonOk.Location = new System.Drawing.Point(417, 634);
+            buttonOk.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
+            buttonOk.Name = "buttonOk";
+            buttonOk.Size = new System.Drawing.Size(133, 50);
+            buttonOk.TabIndex = 50;
+            buttonOk.Text = "Ok";
+            buttonOk.UseVisualStyleBackColor = false;
+            buttonOk.Click += buttonOk_Click;
             // 
             // buttonRem
             // 
@@ -66,28 +69,31 @@
             buttonRem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             buttonRem.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold);
             buttonRem.ForeColor = System.Drawing.Color.White;
-            buttonRem.Location = new System.Drawing.Point(256, 377);
-            buttonRem.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            buttonRem.Location = new System.Drawing.Point(17, 634);
+            buttonRem.Margin = new System.Windows.Forms.Padding(4, 7, 4, 7);
             buttonRem.Name = "buttonRem";
-            buttonRem.Size = new System.Drawing.Size(130, 30);
+            buttonRem.Size = new System.Drawing.Size(186, 50);
             buttonRem.TabIndex = 51;
-            buttonRem.Text = "Remove Packages";
+            buttonRem.Text = "Remove Selected";
             buttonRem.UseVisualStyleBackColor = false;
+            buttonRem.Click += buttonRem_Click;
             // 
             // Modules
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            BackColor = System.Drawing.Color.FromArgb(30, 30, 30);
-            ClientSize = new System.Drawing.Size(398, 420);
+            BackColor = System.Drawing.Color.Gray;
+            ClientSize = new System.Drawing.Size(569, 700);
             Controls.Add(buttonRem);
-            Controls.Add(buttonDeploy);
+            Controls.Add(buttonOk);
             Controls.Add(checkedListBoxModules);
             ForeColor = System.Drawing.Color.Gainsboro;
             FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             Name = "Modules";
             StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            Text = "Modules";
+            Text = "GC Studio Module Packs";
+            FormClosing += Modules_FormClosing;
             Load += Modules_Load;
             ResumeLayout(false);
         }
@@ -97,7 +103,7 @@
         private System.Windows.Forms.CheckedListBox checkedListBoxModules;
         private System.Windows.Forms.Button buttonabout;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button buttonDeploy;
+        private System.Windows.Forms.Button buttonOk;
         private System.Windows.Forms.Button buttonRem;
     }
 }
