8dbcdc46b6ad18ce1e4562a2575ebabebf09d2bd
Angel Mier <angelivan_spartan@hotmail.com>
2025-12-04 16:44:02 -0600

    build for v.1.01.261.301 (for GCdebug Alpha)

--- SRC/GCstudio/Properties/AssemblyInfo.cs
+++ SRC/GCstudio/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.01.26.300")]
-[assembly: AssemblyFileVersion("1.01.26.300")]
+[assembly: AssemblyVersion("1.01.261.301")]
+[assembly: AssemblyFileVersion("1.01.261.301")]
